c82c8c7c4ee9cea6a17ce2a9414fa12ef317c300
ws903 <ws903@nyu.edu>
2018-10-26 21:01:01 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+	def call(env)
+		resp = Rack::Response.new
+		time_now = Time.now.strftime("%H").to_i
+		
+		if time_now < 12
+			resp.write("Good Morning!")
+		else
+			resp.write("Good Afternoon!")
+		end
+		resp.finish
+	end
+end
